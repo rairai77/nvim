453699a7739991b685a3a86e717777822cfc0ad3
Raahil Jain <raahil@Raahils-MacBook-Air.local>
2024-02-29 13:17:54 -0800

    using tab for completions, using alt enter for code actions

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -13,7 +13,9 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
+			local luasnip = require("luasnip")
+			local select_opts = { behavior = cmp.SelectBehavior.Select }
+			require("luasnip.loaders.from_vscode").lazy_load()
 			cmp.setup({
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
@@ -26,11 +28,32 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({ select = false }), -- Set `select` to `false` to only confirm explicitly selected items.
+					["<Up>"] = cmp.mapping.select_prev_item(select_opts),
+					["<Down>"] = cmp.mapping.select_next_item(select_opts),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -28,7 +28,7 @@ return {
             lsconfig.pyright.setup(opts)
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set({ "n", "v" }, "<M-CR>", vim.lsp.buf.code_action, {})
         end,
     },
 }
