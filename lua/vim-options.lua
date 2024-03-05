3e95d922c03e7bf1c1d3d148c596e35c30cbccb1
rairai77 <cutenitki@gmail.com>
2024-03-05 14:38:17 -0800

    added some window navigation commands to leader key

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,6 +8,13 @@ vim.g.mapleader = " "
 --[[ sets line numbers ]]--
 vim.opt.number = true
 vim.opt.relativenumber = true
-
-vim.keymap.set("n", "<leader>wh", ":split<cr>", {desc="horizontal split"})
-vim.keymap.set("n", "<leader>wv", ":vsplit<cr>", {desc="vertical split"})
+--[[ window commands ]]--
+vim.keymap.set("n", "<leader>wsh", ":split<cr>", {desc="horizontal split"})
+vim.keymap.set("n", "<leader>wsv", ":vsplit<cr>", {desc="vertical split"})
+vim.keymap.set("n", "<leader>wt", ":term<cr>", {desc="convert window to terminal"})
+vim.keymap.set("n", "<leader>wh", "<C-w>h", {desc="swap to window left"})
+vim.keymap.set("n", "<leader>wj", "<C-w>j", {desc="swap to window below"})
+vim.keymap.set("n", "<leader>wk", "<C-w>k", {desc="swap to window above"})
+vim.keymap.set("n", "<leader>wl", "<C-w>l", {desc="swap to window right"})
+--[[ terminal commands ]]--
+vim.keymap.set("t", "<esc>", "<C-\\><C-n>", {desc="escape terminal"})
